27e2314fac0f1dce513fbc578a07dd7d3f107229
paulowrld <111534222+paulowrld@users.noreply.github.com>
2023-05-16 10:56:36 -0300

    Add files via upload

---
+++ Questao2/Client-side/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+class HttpClient
+{
+    static void Main(string[] args)
+    {
+        WebClient client = new WebClient();
+
+        string responseString = client.DownloadString("http://localhost:8080/"); // Envia uma solicitação GET para o servidor
+
+        Console.WriteLine(responseString); // Exibe a resposta do servidor
+
+        Console.ReadLine();
+    }
+}
---
+++ Questao2/Server-side/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+class HttpServer
+{
+    static void Main(string[] args)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8080/");
+
+        listener.Start();
+
+        Console.WriteLine("Servidor HTTP iniciado...");
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+
+            // Verifica se a solicitação é uma requisição GET para o diretório raiz do servidor
+            if (request.HttpMethod == "GET" && request.Url.LocalPath == "/")
+            {
+                HttpListenerResponse response = context.Response;
+
+                // Gera a lista de arquivos no diretório raiz do servidor
+                string[] files = Directory.GetFiles(Environment.CurrentDirectory);
+                string fileList = "<ul>";
+                foreach (string file in files)
+                {
+                    fileList += "<li>" + Path.GetFileName(file) + "</li>";
+                }
+                fileList += "</ul>";
+
+                // Cria a resposta HTML
+                string responseString = "<html><body>" + fileList + "</body></html>";
+
+                // Configura a resposta com o conteúdo HTML e os cabeçalhos apropriados
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "text/html";
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            else
+            {
+                // Se a solicitação não é uma requisição GET para o diretório raiz, retorna um erro 404
+                context.Response.StatusCode = 404;
+                context.Response.Close();
+            }
+        }
+    }
+}
